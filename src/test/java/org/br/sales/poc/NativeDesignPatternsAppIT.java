fc6c76bec4b7ca4e92c6d65865a5bc6b5d8128bb
Felipe Sales <felipe.ferreira.sales@gmail.com>
2022-04-09 18:34:39 -0300

    First Commit

---
+++ src/test/java/org/br/sales/poc/NativeDesignPatternsAppIT.java
@@ -0,0 +1,9 @@
+package org.br.sales.poc;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDesignPatternsAppIT extends DesignPatternsAppTest {
+
+    // Execute the same tests but in native mode.
+}
